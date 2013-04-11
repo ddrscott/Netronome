50dbd7111709a96d655ebbb2276bfd101cbd00e6
Scott Pierce <ddrscott@gmail.com>
2013-04-10 23:13:38 -0500

    - removed garbage comments

--- vendor/Audio/AudioHelper.m
+++ vendor/Audio/AudioHelper.m
@@ -1,13 +1,6 @@
-//
-// MBProgressHUD.m
-// Version 0.5
-// Created by Matej Bukovinski on 2.4.09.
-//
-
 #import "AudioHelper.h"
 
 @implementation AudioHelper  {
-//    // class variables here
 }
 
 #pragma mark - Class methods
